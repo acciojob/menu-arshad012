83c2444f12433ce16006105de86233951a7b4afd
arshad012 <forarshad99@gmail.com>
2024-12-20 23:27:33 +0530

    Ids added again

--- src/components/App.js
+++ src/components/App.js
@@ -25,9 +25,9 @@ function App() {
         <h1>Our Menu</h1>
         <div className="filters">
             <button onClick={(e) => handleFilter(e)}>All</button>
-            <button onClick={(e) => handleFilter(e)}>Shakes</button>
-            <button onClick={(e) => handleFilter(e)}>Lunch</button>
-            <button onClick={(e) => handleFilter(e)}>Breakfast</button>
+            <button id="filter-btn-1" onClick={(e) => handleFilter(e)}>Breakfast</button>
+            <button id="filter-btn-2" onClick={(e) => handleFilter(e)}>Lunch</button>
+            <button id="filter-btn-3" onClick={(e) => handleFilter(e)}>Shakes</button>
         </div>
         <div className="dishedContainer">
             {
